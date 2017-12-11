f775cf01cc87ec5d3122529bfcc75d99fb73dd59
sharontz <sharon.tzur@oath.com>
2017-12-11 14:38:16 +0200

    removed unnecessary function for now

--- core/base-component.js
+++ core/base-component.js
@@ -1,39 +1,5 @@
 class BaseComponent extends HTMLElement {
 
-    static polyFill (url) {
-        if (!__flags.isWCSupported) {
-            const existingScript = document.querySelector('script[data-is-slim-polyfill="true"]')
-            if (!existingScript) {
-                const script = document.createElement('script')
-                script.setAttribute('data-is-slim-polyfill', 'true')
-                script.src = url
-                document.head.appendChild(script)
-            }
-        }
-    }
-
-    // Native DOM Api V1
-
-    createdCallback () {
-
-    }
-
-    // Native DOM Api V2
-
-    connectedCallback () {
-
-    }
-
-    disconnectedCallback () {
-
-    }
-
-    attributeChangedCallback(attr, oldValue, newValue) {
-
-    }
-
-    // Slim internal API
-
 }
 
 export default BaseComponent
